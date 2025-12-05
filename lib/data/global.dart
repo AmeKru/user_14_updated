a2d2e141acd04b46f59a343c4255d94e5e469436
amelie <amelie.kruelle@stud.hs-mannheim.de>
2025-12-05 16:20:09 +0800

    startAfternoonService at 11

--- lib/data/global.dart
+++ lib/data/global.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 DateTime? timeNow;
 int startAfternoonETA = 14;
-int startAfternoonService = 9;
+int startAfternoonService = 11;
 int selectedMRT = 0;
 ValueNotifier<int> busIndex = ValueNotifier<int>(0);
 int busMaxCapacity = 30;
